3e53fc3f743c3233893c263457e8a0717c41f8fe
Yaredyy <79294242+Yaredyy@users.noreply.github.com>
2023-05-12 13:52:35 -0400

    added
    model for value to base everything on

---
+++ BudgetBites/app/src/main/java/com/example/budgetbites/adapters/IncomeAdapter.java
@@ -0,0 +1,6 @@
+package com.example.budgetbites.adapters;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
+public class IncomeAdapter extends FirebaseRecyclerAdapter<> {
+}
---
+++ BudgetBites/app/src/main/java/com/example/budgetbites/models/Values.java
@@ -0,0 +1,13 @@
+package com.example.budgetbites.models;
+
+public class Values {
+    public String date;
+    public int value;
+
+    public Values(){}
+
+    public Values(String d, int v){
+        date = d;
+        value = v;
+    }
+}
