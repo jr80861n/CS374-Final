5e02e77de2a65ac4442819f887e940678837bc6b
jr80861n@pace.edu <jr80861n@pace.edu>
2023-05-13 01:28:19 -0400

    quote api

---
+++ BudgetBites/app/src/main/java/com/example/budgetbites/Quote.java
@@ -0,0 +1,28 @@
+package com.example.budgetbites;
+
+public class Quote {
+    private int id;
+    private String advice;
+
+
+    public Quote() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return advice;
+    }
+
+    public void setAdvice(String advice) {
+        this.advice = advice;
+    }
+
+
+}
